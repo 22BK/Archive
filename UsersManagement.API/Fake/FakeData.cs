a3cee0652deae74571cb419c7fd45a95192cd04f
22BK <bugrakirman@outlook.com>
2020-10-07 22:41:25 +0300

    Restful api sample added

---
+++ UsersManagement.API/Fake/FakeData.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersManagement.API.Models;
+
+namespace UsersManagement.API.Fake
+{
+    public static class FakeData
+    {
+        private static List<User> _users;
+
+        public static List<User> GetUser(int number)
+        {
+            if (_users == null)
+            {
+                _users = new Faker<User>()
+                                .RuleFor(x => x.Id, z => z.IndexFaker+1)
+                                .RuleFor(x => x.FirstName, z => z.Name.FirstName())
+                                .RuleFor(x => x.LastName, z => z.Name.LastName())
+                                .RuleFor(x => x.Address, z => z.Address.FullAddress())
+                                .Generate(number);
+            }
+            
+            return _users;
+        }
+    }
+}
