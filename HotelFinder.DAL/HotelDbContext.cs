e1762c147850c437de57556c9e7906b15559b89c
22BK <bugrakirman@outlook.com>
2020-10-08 00:21:37 +0300

    CRUD operations with EFCore added

---
+++ HotelFinder.DAL/HotelDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelFinder.DAL
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=BUGRAK;Initial Catalog=HotelDB;Integrated Security=True;");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
